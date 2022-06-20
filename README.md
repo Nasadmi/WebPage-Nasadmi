e4f124f6ab239665b1bcdb51f908f144baa4a006
Nasadmi <98781576+Nasadmi@users.noreply.github.com>
2022-06-20 19:17:07 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# WebPage
+### WebPage with **HTML** ,**CSS** and **JavaScript**, the **HTML** and **JavaScript** have a very easy code but the **CSS** is very long and some difficult of the rest code. This is code **JavaScript** for the elements appear in **HTML**.
+
+```javascript
+
+document.getElementbyId("ElementId").style.display = "block or none"
+
+```
+
+### This is a Finally Result
+
+![](
+    ./index.png
+)
---
+++ WebPage/javascript/main.js
@@ -0,0 +1,37 @@
+//Show
+function show() {
+    document.getElementById("hide").style.display = "block";
+}
+//Hide
+function hide() {
+    document.getElementById("hide").style.display = "none"
+}
+
+//World
+
+function showtechnology() {
+    document.getElementById("world").style.display = "block";
+}
+
+function hidetechnology() {
+    document.getElementById("world").style.display = "none"
+}
+
+//Biology
+function showbiology() {
+    document.getElementById("biology").style.display = "block";
+}
+
+function hidebiology() {
+    document.getElementById("biology").style.display = "none"
+}
+
+//Scientist
+
+function showscientist() {
+    document.getElementById("scientist").style.display = "block";
+}
+
+function hidescientist() {
+    document.getElementById("scientist").style.display = "none"
+}
